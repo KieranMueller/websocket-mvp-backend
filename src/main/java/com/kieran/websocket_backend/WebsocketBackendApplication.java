0a769cdfda4b381d7d63a2c034a7497f3a329a3c
KieranMueller <kieran98mueller@gmail.com>
2024-08-15 13:48:32 -0500

    init

---
+++ src/main/java/com/kieran/websocket_backend/WebsocketBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kieran.websocket_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kieran/websocket_backend/WebsocketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kieran.websocket_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
